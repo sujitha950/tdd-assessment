7e63362d9d59ed5280fa8d569e1f776341d22404
sujitha950 <67088002+sujitha950@users.noreply.github.com>
2025-02-19 22:23:15 +0530

    Update addfunctionality.py
    Added logic to handle new line

--- addfunctionality.py
+++ addfunctionality.py
@@ -1,6 +1,7 @@
 def add_numbers(numbers):
     if not numbers:
         return 0
+        numbers = numbers.replace('\n', ',')
         nums = [int(n) for n in numbers.split(',')]
     return sum(nums)
     
@@ -9,6 +10,7 @@ def test_add():
     assert add("") == 0
     assert add("1") == 1
     assert add("1,5") == 6
-    print("Added comma seperated numbers are passed")
+    assert add("1\n2,3") == 6
+    print("Handled new line logic here")
 
 test_add()
