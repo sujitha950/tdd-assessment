4f15d90f1fa1209f2d7e60a740459dd73fa89765
sujitha950 <67088002+sujitha950@users.noreply.github.com>
2025-02-19 22:16:43 +0530

    Update addfunctionality.py
    Basic strings and single numbers are passed

--- addfunctionality.py
+++ addfunctionality.py
@@ -1,1 +1,11 @@
+def add_numbers(numbers):
+    if not numbers:
+        return 0
+    return int(numbers)
 
+def test_add():
+    assert add("") == 0
+    assert add("1") == 1
+    print("Basic strings and single numbers are passed")
+
+test_add()
