91818871ffa45f3ba378e366e6f9e93e9807def0
sujitha950 <67088002+sujitha950@users.noreply.github.com>
2025-02-19 22:19:55 +0530

    Update addfunctionality.py
    Added comma seperated numbers logic

--- addfunctionality.py
+++ addfunctionality.py
@@ -1,11 +1,14 @@
 def add_numbers(numbers):
     if not numbers:
         return 0
-    return int(numbers)
+        nums = [int(n) for n in numbers.split(',')]
+    return sum(nums)
+    
 
 def test_add():
     assert add("") == 0
     assert add("1") == 1
-    print("Basic strings and single numbers are passed")
+    assert add("1,5") == 6
+    print("Added comma seperated numbers are passed")
 
 test_add()
